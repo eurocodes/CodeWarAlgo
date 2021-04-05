feda3a9c5765cc25407557ea2f91ccc3f6074e3f
Ugo Umeoke <laryhug209@yahoo.com>
2021-04-05 20:26:47 +0100

    Add project files.

---
+++ CodeWarAlgo/Mon_5th_April/CreatePhone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarAlgo.Mon_5th_April
+{
+    class CreatePhone
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            string phone = "(";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i < 2)
+                {
+                    phone += numbers[i];
+                }
+                else if (i == 2)
+                {
+                    phone += numbers[i] + ") ";
+                }
+                else if (i> 2 && i < 5)
+                {
+                    phone += numbers[i];
+                }
+                else if (i == 5)
+                {
+                    phone += numbers[i] + "-";
+                }
+                else
+                {
+                    phone += numbers[i];
+                }
+            }
+            return phone;
+        }
+
+        public static string CreatePhonenum(int[] number)
+        {
+            return $"({number[0]}{number[1]}{number[2]}) " +
+                $"{number[3]}{number[4]}{number[5]}-{number[6]}{number[7]}{number[8]}{number[9]}";
+        }
+    }
+}
---
+++ CodeWarAlgo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CodeWarAlgo.Mon_5th_April;
+
+namespace CodeWarAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreatePhone.CreatePhonenum(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
